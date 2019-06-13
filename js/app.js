86754d2903dcbb8568f47b423351a0420d1c7a82
mercyikpe <ikpemercy1@gmail.com>
2019-06-13 15:15:23 +0100

    option for year set to 20years

--- js/app.js
+++ js/app.js
@@ -20,6 +20,19 @@ function HTMLUI() {}
 HTMLUI.prototype.displayYears = function() {
   // Max and Min years
   const max = new Date().getFullYear();
-        // min = max - 20;
-  console.log(max);
+    min = max - 20;
+
+
+  // Generate the list with the latest 20 years
+  const selectYears = document.getElementById('year');
+
+  // Print the values
+  for(let i = max; i >= min; i-- ) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = i;
+    selectYears.appendChild(option); 
+  }
 } 
+
+
