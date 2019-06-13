f208531db6c1faa3e8dccd8ad6014bfa1f4da6c7
mercyikpe <ikpemercy1@gmail.com>
2019-06-13 15:38:00 +0100

    event listeners created for form values

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
 // variables
+const form = document.getElementById('request-quote');
+
 
 
 
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', function() {
-  // create the <option> for the years
-  const html = new HTMLUI();
-  html.displayYears();
-});
+eventListeners();
+function eventListeners() {
+  document.addEventListener('DOMContentLoaded', function() {
+    // create the <option> for the years
+    const html = new HTMLUI();
+    html.displayYears();
+  });
+
+  // when form is submitted
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Read the values from the form
+    const make = document.getElementById('make').value;
+    const year = document.getElementById ('year').value;
+    // Read the radio buttons
+    const level = document.querySelector('input[name="level"]:checked').value;
+    // check that all the fields have something
+
+    if( make === '' || year === '' || level === '') {
+
+    }
+  })
+
+}
+
+
+
+
+
+
 
 
 
